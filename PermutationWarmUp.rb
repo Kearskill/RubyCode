5266f943e3e726c514ed11c053e893d77c6a645a
Kearskill <kearskill@gmail.com>
2025-05-20 22:52:53 +0800

    I like red ruby

---
+++ PermutationWarmUp.rb
@@ -0,0 +1,10 @@
+# https://codeforces.com/problemset/problem/2108/A
+# puts 'Input number of test cases:'
+n = gets.to_i
+
+n.times do
+    temp = gets.to_i
+    res = (temp**2) / 4.0 + 1
+    res = res.to_i
+    puts res
+end
---
+++ ex2.rb
@@ -0,0 +1,18 @@
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+puts "This will run."
+
+# the # character is called octothorpe or pound character
+
+x = 4
+while x>=1
+    puts "Skibidi" 
+    x -= 1
+end
+
+print "hellow world"
